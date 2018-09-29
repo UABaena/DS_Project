05c46470951622cfb3f56b8714fe29d151004808
lorenatortajada16 <lorenatortajadaropero@gmail.xom>
2018-09-29 14:29:22 +0200

    prova2

--- app/src/main/java/com/example/vbaenach/dsproject/Interval.java
+++ app/src/main/java/com/example/vbaenach/dsproject/Interval.java
@@ -1,4 +1,25 @@
 package com.example.vbaenach.dsproject;
 
-public class Interval {
-}
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Date;
+
+public class Interval implements Observer {
+
+
+    void Interval() {
+
+    }
+
+
+    @Override
+    public void update(Observable arg0, Object arg1) {
+        Date presentDate = (Date) arg1;
+        System.out.println(presentDate);
+    }
+
+
+};
+
+
+
--- app/src/main/java/com/example/vbaenach/dsproject/MainApplication.java
+++ app/src/main/java/com/example/vbaenach/dsproject/MainApplication.java
@@ -6,6 +6,19 @@ public class MainApplication {
 
 
         System.out.println("Hello funciona!");
+
+
+        Interval observador = new Interval();
+        Clock clock= new Clock();
+        clock.addObserver(observador);
+        clock.tick();
+
+
+
+
+
+        }
     }
 
-}
+
+
