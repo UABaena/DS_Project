0049a92d23fccc71b4feedf84e36bbd36df3d622
lorenatortajada16 <lorenatortajadaropero@gmail.xom>
2018-10-02 14:35:16 +0200

    initial Date en Interval

--- app/src/main/java/com/example/vbaenach/dsproject/Interval.java
+++ app/src/main/java/com/example/vbaenach/dsproject/Interval.java
@@ -6,6 +6,9 @@ import java.util.Date;
 
 public class Interval implements Observer {
 
+    Date initalDate;
+    Date finalDate;
+
 
     public  Interval(Clock clock) {
         clock.addObserver(this);
@@ -15,7 +18,14 @@ public class Interval implements Observer {
 
     @Override
     public void update(Observable arg0, Object arg1) {
+        int k=0;
         Date presentDate = (Date) arg1;
+
+        if (k==0)
+        {
+            this.initalDate=presentDate;
+            k++;
+        }
         System.out.println("Data a l'observer");
         System.out.println(presentDate);
     }
